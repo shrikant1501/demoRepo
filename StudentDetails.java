b1099db6ae9306f420433216c186d68b458416d1
Shrikant Thakre <shrikant.thakre@ibm.com>
2022-06-06 12:41:57 +0530

    class inherited

--- StudentDetails.java
+++ StudentDetails.java
@@ -23,3 +23,22 @@ class StudentDetails
 	}
 
 }
+class Student extends StudentDetails 
+{
+
+	void display() 
+	{
+		System.out.println("/******* Student details printed ********/");
+		System.out.println("Roll Number is: "+roll_no);
+		System.out.println("Your name is: "+name);
+		System.out.println("Your class is: "+cl);
+	}
+
+	public static void main(String args[]) 
+	{
+		Student obj = new Student();
+		obj.input();
+		obj.display();
+	}
+
+}
